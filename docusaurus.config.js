28de2e11a394fe912f6d81aeac3b62f07e8e72c1
Dcose <17873001707@163.com>
2022-09-17 22:53:12 +0800

    🎉 init: init

---
+++ docusaurus.config.js
@@ -0,0 +1,93 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Kuriyama',
+  tagline: '将来のない未来は、私の望みの未来ではありません',
+  url: 'https://dcose.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  plugins: ['@docusaurus/theme-live-codeblock'],
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Dcose', // Usually your GitHub org/user name.
+  projectName: 'dcose', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Kuriyama的博客',
+        hideOnScroll: false,
+        items: [
+          {
+            type: 'search',
+            position: 'right',
+          },
+          {
+            type: 'doc',
+            docId: 'HTML',
+            position: 'right',
+            label: '正文',
+          },
+          {
+            href: 'https://github.com/Dcose',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+        defaultLanguage: 'javascript',
+      },
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+    }),
+};
+
+module.exports = config;
