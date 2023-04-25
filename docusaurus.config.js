09bc0e7a8f51fba8735bef48bab0c5edd3aa3506
Dcose <17873001707@163.com>
2023-04-25 23:23:40 +0800

    🎉 init: init

---
+++ docusaurus.config.js
@@ -0,0 +1,117 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+	title: 'Mirai', // blog 标题
+	tagline: '将来のない未来は、私の望みの未来ではありません', // 想说的话
+	favicon: 'img/favicon.ico', // 书签图标
+
+	// Set the production url of your site here
+	url: 'https://dcose.github.io',
+
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '/',
+
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'Dcose', // Usually your GitHub org/user name.
+	projectName: 'dcose', // Usually your repo name.
+	deploymentBranch: 'gh-pages',
+
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'zh-Hans',
+		locales: ['zh-Hans'],
+	},
+
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl:
+						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
+
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			// Replace with your project's social card
+			// image: 'img/docusaurus-social-card.jpg',
+			navbar: {
+				title: 'Mirai的博客',
+				hideOnScroll: true,
+				// logo: {
+				// 	alt: 'My Site Logo',
+				// 	src: 'img/logo.svg',
+				// },
+				items: [
+					// {
+					// 	type: 'docSidebar',
+					// 	sidebarId: 'tutorialSidebar',
+					// 	position: 'left',
+					// 	label: 'Tutorial',
+					// },
+					// { to: '/blog', label: 'Blog', position: 'left' },
+					// {
+					// 	href: 'https://github.com/facebook/docusaurus',
+					// 	label: 'GitHub',
+					// 	position: 'right',
+					// },
+					{
+						type: 'search',
+						position: 'right',
+					},
+					{
+						type: 'doc',
+						docId: 'HTML',
+						position: 'right',
+						label: '正文',
+					},
+					{
+						href: 'https://github.com/Dcose',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+				defaultLanguage: 'javascript',
+			},
+			colorMode: {
+				defaultMode: 'light',
+				respectPrefersColorScheme: true,
+			},
+		}),
+};
+
+module.exports = config;
